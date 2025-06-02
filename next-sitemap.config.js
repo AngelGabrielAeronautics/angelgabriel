24a724f4925fd2cc3bf99cb405b06911be34d9c5
AngelGabrielAeronautics <dylan@angelgabriel.uk>
2025-06-02 10:03:17 +0100

    Update site URL and sitemap entries to reflect new domain. Remove deprecated 'Get a Quote' page and adjust robots.txt accordingly.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || process.env.NEXT_PUBLIC_SITE_URL || 'https://angelgabriel.co.za',
+  siteUrl: process.env.SITE_URL || process.env.NEXT_PUBLIC_SITE_URL || 'https://flyangelgabriel.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.8,
